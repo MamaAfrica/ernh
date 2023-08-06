ea9fec7d2ee22a80135ac80e5d845b8c74e757d0
jegbogu <jegbogu@gmail.com>
2023-08-06 01:36:18 +0100

    more work

--- component/adminDashboard/withdrawalActivation.js
+++ component/adminDashboard/withdrawalActivation.js
@@ -1,5 +1,46 @@
-function WithdrawalActivation(){
-    return(
+import { useRef } from "react";
+import { useState } from "react";
+import { useRouter } from 'next/router';
+
+import classes from '../../component/users/withdrawalForm.module.css'
+
+
+function WithdrawalActivation() {
+    const [confirmMsg, setConfirmMsg] = useState(false)
+
+    const activeInputRef = useRef()
+    const confirmInputRef = useRef()
+    const router = useRouter()
+
+    if (confirmInputRef !=='YES') {
+        setConfirmMsg(<h3> You are no longer going ahead with the action</h3>)
+        return
+    }else{
+        setConfirmMsg(<h3>Deactivating..</h3>)
+    }
+    
+
+    async function submitHandler() {
+        const response = await fetch('api/deactivateWithdrawal/deactivateWithdrawal', {
+            method: 'POST',
+            body: JSON.stringify({ activeInputRef, confirmInputRef }),
+            headers: {
+                'Content-type': 'application/json'
+            },
+
+        });
+        let user = await response.json()
+
+        if (!response.ok) {
+            throw new Error(user.message || 'something went wrong')
+        }
+
+        router.reload()
+    }
+
+
+
+    return (
         <div className={classes.section}>
 
             <div className={classes.card}>
@@ -9,15 +50,15 @@ function WithdrawalActivation(){
 
 
                         <select
-                            ref={withdrawalInputRef}
+                            ref={activeInputRef}
                             required
                             id="withdrawal"
                             name="withdrawal"
                         >
-                            <option>Activate Withdrawal</option>
-                            <option>De-Activate Withdrawal</option>
-                            
-                           
+                            <option value='activate'>Activate Withdrawal</option>
+                            <option value='deactivate'>De-Activate Withdrawal</option>
+
+
                         </select>
 
                     </div>
@@ -36,7 +77,7 @@ function WithdrawalActivation(){
 
                     </div>
 
-                        {confirmErr}
+                    {confirmMsg}
                     <div className={classes.actions}>
                         <button type="submit">Submit a Request</button>
                     </div>
